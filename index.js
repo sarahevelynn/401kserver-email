b35d603efc4f3b22779d38cdcc305c99fce41cde
Patrick Biffle <pbiffle@gmail.com>
2018-03-15 17:07:08 -0600

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // app.use(express.static('./public'))
