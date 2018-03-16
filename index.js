0dc2107fd96ea4398eb73b5cdc95cf0fce9ae76a
Patrick Biffle <pbiffle@gmail.com>
2018-03-16 10:41:32 -0600

    replaced everthing with cj's code

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-// app.use(express.static('./public'))
+app.use(express.static('./public'))
+app.use(cors())
 
 const mailer = require('./mailer')
 
@@ -17,21 +17,29 @@ app.post('/send', (req, res) => {
 	const message = {
 		from: process.env.FROM_EMAIL,
 		to: process.env.TO_EMAIL,
-		subject: 'site contact form',
-		text: `From ${req.body.email}\n Sent: ${new Date()} \n Name: ${req.body.name} \n Message: \n ${
+		subject: 'Site Contact Form',
+		text: `From: ${req.body.email}\n Sent: ${new Date()} \n Name: ${req.body.name} \n Message: ${
 			req.body.message
 		}`
 	}
 
-	mailer.sendMessage(message).then(() => {
-		res.json({
-			message: 'Mail Sent!'
+	mailer
+		.sendMessage(message)
+		.then(() => {
+			res.json({
+				message: 'Email sent.'
+			})
 		})
-	})
+		.catch(error => {
+			res.status(500)
+			res.json({
+				error: error
+			})
+		})
 })
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-	console.log(`listening on ${port}`)
+	console.log(`Listening on ${port}`)
 })
