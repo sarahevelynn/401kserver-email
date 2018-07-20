af67c07d5c70433e1973d4e6b84da1dbd80c6cd2
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-07-20 13:47:44 -0600

    initial data upload

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.static('./public'))
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+app.use(cors());
 
-const mailer = require('./mailer')
+const mailer = require("./mailer");
 
-app.post('/send', (req, res) => {
-	const message = {
-		from: process.env.FROM_EMAIL,
-		to: process.env.TO_EMAIL,
-		subject: 'Site Contact Form',
-		text: `From: ${req.body.email}\n Sent: ${new Date()} \n Name: ${req.body.name} \n Message: ${
-			req.body.message
-		}`
-	}
+app.post("/send", (req, res) => {
+  const message = {
+    from: process.env.FROM_EMAIL,
+    to: process.env.TO_EMAIL,
+    subject: "Site Free Guide Form",
+    text: `From: ${req.body.signupEmail}\n
+			Sent: ${new Date()} \n
+			Name: ${req.body.signupName} \n
+		Phone: ${req.body.signupPhone} \n
+		Company Name: ${req.body.signupCoName} \n
+		Number of Employees: ${req.body.EmployeeNumber} \n `
+  };
 
-	mailer
-		.sendMessage(message)
-		.then(() => {
-			res.json({
-				message: 'MESSAGE SENT!'
-			})
-		})
-		.catch(error => {
-			res.status(500)
-			res.json({
-				error: error
-			})
-		})
-})
+  mailer
+    .sendMessage(message)
+    .then(() => {
+      res.json({
+        message: "MESSAGE SENT!"
+      });
+    })
+    .catch(error => {
+      res.status(500);
+      res.json({
+        error: error
+      });
+    });
+});
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-	console.log(`Listening on ${port}`)
-})
+  console.log(`Listening on ${port}`);
+});
