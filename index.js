bf2421016486552cadf11b89c35701333e086dc5
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-07-20 14:51:23 -0600

    example test

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var nodemailer = require("nodemailer");
-const creds = require("../config/config");
+var nodemailer = require('nodemailer');
+const creds = require('../config/config');
 
 var transport = {
-  host: "smtp.gmail.com",
+  host: 'smtp.gmail.com',
   auth: {
     user: process.env.GMAIL_ACCOUNT,
-    pass: process.env.GMAIL_PASSWORD
-  }
-};
+		pass: process.env.GMAIL_PASSWORD  }
+}
 
-var transporter = nodemailer.createTransport(transport);
+var transporter = nodemailer.createTransport(transport)
 
 transporter.verify((error, success) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Server is ready to take messages");
+    console.log('Server is ready to take messages');
   }
 });
 
-router.post("/send", (req, res, next) => {
-  var content = `From: ${req.body.signupEmail}\n
-    Sent: ${new Date()} \n
-    Name: ${req.body.signupName} \n
-  Phone: ${req.body.signupPhone} \n
-  CompanyName: ${req.body.signupCoName} \n
-  NumberofEmployees: ${req.body.EmployeeNumber} \n `;
+router.post('/send', (req, res, next) => {
+  var name = req.body.name
+  var email = req.body.email
+  var message = req.body.message
+  var content = `name: ${name} \n email: ${email} \n message: ${content} `
 
   var mail = {
-    from: process.env.FROM_EMAIL,
-    to: process.env.TO_EMAIL,
-    subject: "Site Free Guide Form",
+    from: name,
+    to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',  //Change to email address that you want to receive messages on
+    subject: 'New Message from Contact Form',
     text: content
-  };
+  }
 
   transporter.sendMail(mail, (err, data) => {
     if (err) {
       res.json({
-        msg: "fail"
-      });
+        msg: 'fail'
+      })
     } else {
       res.json({
-        msg: "success"
-      });
+        msg: 'success'
+      })
     }
-  });
-});
+  })
+})
 
 module.exports = router;
