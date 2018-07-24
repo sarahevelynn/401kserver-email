b2fcbc46aa70c85e483070f48772e69d8dfd988a
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-07-24 13:43:29 -0600

    upload

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const mailer = require("./mailer");
 
 app.post("/send", (req, res) => {
   const message = {
-    from: process.env.FROM_EMAIL,
-    to: process.env.TO_EMAIL,
     subject: "Site Contact Form",
     text: `From: ${req.body.email}\n Sent: ${new Date()} \n Name: ${
       req.body.name
