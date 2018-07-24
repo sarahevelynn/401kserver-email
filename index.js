25dd478f4b05a42bfb5eeb8203c3f65a9c72ffb3
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-07-24 14:05:18 -0600

    upload

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
-var express = require("express");
-var router = express.Router();
-var nodemailer = require("nodemailer");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("dotenv").config();
 
-let transport = {
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.GMAIL_ACCOUNT,
-    pass: process.env.GMAIL_PASSWORD
-  }
-};
+const app = express();
 
-let transporter = nodemailer.createTransport(transport);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+app.use(cors());
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take messages");
-  }
-});
+const mailer = require("./mailer");
 
-router.post("/send", (req, res, next) => {
-  var name = req.body.name;
-  var email = req.body.email;
-  var message = req.body.message;
-  var content = `name: ${name} \n email: ${email} \n message: ${content} `;
-
-  var mail = {
-    from: name,
-    to: "RECEIVING_EMAIL_ADDRESS_GOES_HERE", //Change to email address that you want to receive messages on
-    subject: "New Message from Contact Form",
-    text: content
+app.post("/send", (req, res) => {
+  const message = {
+    subject: "Site Contact Form",
+    text: `From: ${req.body.email}\n Sent: ${new Date()} \n Name: ${
+      req.body.name
+    } \n Message: ${req.body.message}`
   };
 
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
+  mailer
+    .sendMessage(message)
+    .then(() => {
       res.json({
-        msg: "fail"
+        message: "MESSAGE SENT!"
       });
-    } else {
+    })
+    .catch(error => {
+      res.status(500);
       res.json({
-        msg: "success"
+        error: error
       });
-    }
-  });
+    });
 });
 
-module.exports = router;
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
