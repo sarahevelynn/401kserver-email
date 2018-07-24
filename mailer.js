9cb39fc5115c1514f7d96b369d6381898df5e188
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-07-24 15:04:28 -0600

    upload

--- mailer.js
+++ mailer.js
@@ -14,14 +14,6 @@ let transport = {
 
 let transporter = nodemailer.createTransport(transport)
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
-});
-
 module.exports = {
 	sendMessage(message) {
 		return new Promise((resolve, reject) => {
